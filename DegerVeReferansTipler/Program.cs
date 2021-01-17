c778828997211fd2ac14062760a8ea44513bd64b
Muhammet Furkan Kaya <mfrknkya@gmail.com>
2021-01-18 02:02:21 +0300

    16 Ocak ders sonu

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int sayi1 = 10;                       
+            int sayi2 = 30;                       //int,decimal,float,double,bool(arka planda 0,1 (true,false) tutar) = değer tip
+            sayi1 = sayi2;                                 //stock' a atar
+            sayi2 = 65;                                  
+            Console.WriteLine(sayi1); //30
+
+            int[] sayilar1 = new int[] {10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };       // array,class,interface = referans tip
+            sayilar1 = sayilar2;                                     //heap' e atar
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]); //999
+        }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kurs1 = "Yazılım Geliştirici Yerleştirme Kampı";
+            string kurs2 = "Programlamaya Başlangıç İçin Temel kurs";
+            string kurs3 = "java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yerleştirme Kampı",
+                "Programlamaya Başlangıç İçin Temel kurs",
+                "java","Python","C#" };
+
+            for (int i = 0; i <kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("for bitti");
+
+            foreach (string kurs in kurslar) //genelde foreach kullanılır.
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
