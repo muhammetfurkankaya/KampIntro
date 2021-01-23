bc0e5c0d5e0cf67415a7d644f87efd23f7d53b0f
Muhammet Furkan Kaya <mfrknkya@gmail.com>
2021-01-23 17:28:55 +0300

    C# Alıştırma

--- Metotlar/SepetManager.cs
+++ Metotlar/SepetManager.cs
@@ -19,5 +19,6 @@ namespace Metotlar
             Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
         }
 
+
     }
 }
---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    //snippet(prop etc)
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
+        //CRUD
+    }
+}
